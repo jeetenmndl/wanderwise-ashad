daf10883d6854bce2a3e5e1ae2a09523942e9c25
jeetenmndl <jeetenmndl96@gmail.com>
2026-08-20 17:59:50 +0545

    feat: add UI components and update styles
    - Added Button component with variants using class-variance-authority.
    - Introduced HoverCard component for displaying content on hover.
    - Updated index.css to include new styles and themes.
    - Created utility functions for class name merging.
    - Enhanced Contact page with new Button and HoverCard components.
    - Updated package.json to include new dependencies for UI components.
    - Configured Vite to resolve paths for easier imports.

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import Navbar from '../components/common/Navbar'
+import { Button } from '../components/ui/button'
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card"
 
 const Contact = () => {
   return (
     <div>
-        <Navbar />
+      <Navbar />
+
+      <Button>Click me</Button>
+      <Button variant='outline' className="bg-destructive">Click me</Button>
+
+      <HoverCard>
+        <HoverCardTrigger>Hover</HoverCardTrigger>
+        <HoverCardContent>
+          
+          <img src='/heroImage.jpg' alt="dfdfd" />
+        </HoverCardContent>
+      </HoverCard>
+
     </div>
   )
 }
